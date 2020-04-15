5b5fdb81ff74b6affdf2f669c96ec77e9c45aeda
favio <favio@LAPTOP-K9NVDQHN>
2020-04-14 21:43:21 -0400

    update

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link href="css/stile_index.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title>Aperture science spa</title>
+        
+    </head>
+    
+    <body id="body">
+        <header id="cabezera">
+            <br>
+            <center><h1 style="color: white">Tecnolog&iacute;a en Pistolas de portales </h1></center>
+            <br>
+        </header>
+       
+        <nav id="menu">
+            <ul>
+                <li><a href="#">inicio</a></li>
+                <li><a href="#">nuevas tecnolog&iacute;as</a></li>
+                <li><a href="#">contacto</a></li>
+            </ul>
+        </nav>
+        
+        <table id="tabla" border="1" width="1500">
+            <tr>
+                <td height="500" width="1200">
+        <section>
+            
+            <h1>Dispositivo Portátil de Portales de Aperture Science</h1>
+            <div align=center><img src="imagen/pistola.jpg" alt=""  /></div>
+            <p>El Dispositivo Portátil de Portales de Aperture Science,<br>
+                también conocido comúnmente como Pistola de Portales o por<br>
+                sus siglas en inglés, "ASHPD" (Aperture Science Handheld <br>               
+                Portal Device), es una herramienta experimental utilizada <br>
+                para crear dos portales a través de la cual los objetos <br>
+                pueden pasar. Se utiliza en las cámaras de prueba del Centro<br>
+                de Desarrollo de Aperture Science, es la herramienta principal <br>
+               que el Sujeto de Prueba debe utilizar para completar cada prueba.
+            </p>
+            <p>
+                La Pistola de Portales es un dispositivo que tiene la capacidad <br>
+                de fabricar dos portales enlazados. No importa la distancia entre <br>
+                ellos, cualquier objeto que pase a través de un portal saldrá del <br>
+                otro y viceversa instantáneamente. Los portales se pueden colocar <br>
+                en cualquier superficie que sea manufacturada o refinada con roca <br>
+                lunar y sea lo suficientemente grande para acomodarse a ella. Objetos,<br>
+                o ciertos tipos de superficies en movimiento, generalmente previenen la <br>
+                formación de un portal. La Red de Emancipación Material de Aperture <br>
+                Science bloqueará cualquier intento de disparar portales a través <br>
+                de ella, así como desactivara los portales activos si el ASHPD pasa <br>
+                a través de élla.
+            </p>
+            
+        </section>
+                </td>
+                <td  width="300">
+
+            <aside>
+                <h1>nuestras redes Sociales</h1>
+            <ul>
+                <li><a href="#">Facebook</a></li>
+                <img src="imagen/facebook-3-xxl.png" alt=""/>
+                <li><a href="#">Instagram</a></li>
+                <img src="imagen/instagram2.png" alt=""/>
+                <li><a href="#">Twitter</a></li>
+                <img src="imagen/icono-twitter.png" alt=""/>
+            </ul>
+            </aside>
+               </td>
+            </tr>
+        </table>
+        <footer id="pie_dePagina">
+            <br>
+            <br>
+            <center>
+                <h5 style="color: white">contacto</h5>
+                <p style="color: white">tel&eacute;fono:22552536<br>
+                  e-mail:          
+            
+            <a href="#">contacto@aperturescience.com</a>
+            </p>
+            </center>
+            <br>
+            <br>
+            <br>
+        </footer>
+    </body>
+</html>
